8080909598a1cdbb623d1e5f7106e594f8ac179b
s-lin <slin07@risd.edu>
2017-11-30 12:26:33 -0500

    4th commit

--- jelly/jellystyle.js
+++ jelly/jellystyle.js
@@ -1,19 +1,16 @@
-$(document).ready(function() { 
-	$('.button1').click(function(){
-		$(this).addClass('animate');
-	$('.button1').mouseover(function function_name(argument) {
-		// body...
-	})
-	});
-
-	var audioElement = document.createElement('audio');
+$(document).ready(function() {
+    var audioElement = document.createElement('audio');
     audioElement.setAttribute('src', 'jelly_audio/double_slurp.mp3');
     
     audioElement.addEventListener('ended', function() {
         this.play();
     }, false);
     
-    $('button2').click(function() {
+    $('#button2').click(function() {
         audioElement.play();
+    });
+    
+    $('#pause').click(function() {
+        audioElement.pause();
     });
 });
